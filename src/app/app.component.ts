9e0e56a8beaf7b6335fdb3706fe9edf90a8a6054
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-05 13:33:31 -0600

    Create light, dark, buy, and sell themes and CSS rules to switch.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatSliderModule} from "@angular/material/slider";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSliderModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'themed-buy-sell';
+
+  isSell: boolean = false;
+
+  units = 1;
+  unitsMin = 1;
+  unitsMax = 10;
+  unitsStep = 1;
+
+  price = 500;
+  priceMin = 0;
+  priceMax = 1000;
+  priceStep = 25;
+
+  buyColor() {
+    return this.isSell ? '' : 'buy';
+  }
+
+  sellColor() {
+    return this.isSell ? 'sell' : '';
+  }
+
+  color() {
+    return this.isSell ? 'sell' : 'buy';
+  }
+
+  colorSlider() {
+    return this.isSell ? 'sell-slider' : 'buy-slider';
+  }
+
+  colorMaterial() {
+    return this.isSell ? 'primary' : 'accent';
+  }
+
 }
