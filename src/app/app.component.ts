cc9f44e9486aee2e46ab0b448d0640c4eefb8238
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-06 09:09:05 -0600

    Move components into directory; add header and footer stubs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {OrderComponent} from "./order/order.component";
+
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { OrderComponent } from "./components/order/order.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    FooterComponent,
+    HeaderComponent,
     OrderComponent,
   ],
   templateUrl: './app.component.html',
