ad2e3f5059f02494d229bc150386697170450f80
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-15 11:14:01 -0600

    Rearrange with custom sidenav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { OrderComponent } from "./components/order/order.component";
+import {MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {SidenavComponent} from "./components/sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
+
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+
     FooterComponent,
     HeaderComponent,
     OrderComponent,
+    SidenavComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-sidenav',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+
+}
