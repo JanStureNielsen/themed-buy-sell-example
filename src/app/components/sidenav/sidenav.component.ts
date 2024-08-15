ced756fbb33e6ec8dc23aff99001e424d69f61fc
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-15 15:26:40 -0600

    Add sidenav items via signals.

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+
+}
 
 @Component({
   selector: 'fm-sidenav',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatListModule
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'videos'
+    },
+    {
+      icon: 'dashboard',
+      label: 'Analytics',
+      route: 'analytics'
+    }
+  ]);
 }
