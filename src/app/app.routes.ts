1bcb29af85e9b62473af2db848e16e9d90778d10
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-18 10:42:29 -0600

    Add routes to pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {AnalyticsComponent} from "./pages/analytics/analytics.component";
+import {ContentComponent} from "./pages/content/content.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'content',
+    component: ContentComponent,
+  },
+  {
+    path: 'analytics',
+    component: AnalyticsComponent,
+  }
+];
