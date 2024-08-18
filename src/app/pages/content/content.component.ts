6014fad29c76ec6806395c6d9d4a6f0d1df42c5a
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-18 12:54:37 -0600

    Add order component to content page.

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {OrderComponent} from "../../components/order/order.component";
 
 @Component({
   selector: 'fm-content',
   standalone: true,
-  imports: [],
+  imports: [
+    OrderComponent
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
