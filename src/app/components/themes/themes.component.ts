bde20d6f9089e417ca409934e26d4edf1b78399a
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-18 15:09:29 -0600

    Adding FormsModule for the NgModel support.

--- src/app/components/themes/themes.component.ts
+++ src/app/components/themes/themes.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'fm-themes',
   standalone: true,
     imports: [
-        MatButtonToggleModule,
+      FormsModule,
+      MatButtonToggleModule,
+
     ],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss'
