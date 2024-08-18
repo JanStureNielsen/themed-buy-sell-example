d2ab7e6d2f3ba3d45a27d6ce6a7ee55f74f9d110
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-18 14:42:26 -0600

    Add button group toggle for light/dark theme.

--- src/app/components/themes/themes.component.ts
+++ src/app/components/themes/themes.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 @Component({
   selector: 'fm-themes',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButtonToggleModule,
+    ],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss'
 })
 export class ThemesComponent {
+  lightTheme = false;
 
 }
