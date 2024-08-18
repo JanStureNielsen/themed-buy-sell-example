d812c10d208f6f380c48c0998e846bcc58bca0fa
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-18 15:05:18 -0600

    Add settings route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {AnalyticsComponent} from "./pages/analytics/analytics.component";
 import {ContentComponent} from "./pages/content/content.component";
+import {SettingsComponent} from "./pages/settings/settings.component";
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'analytics',
     component: AnalyticsComponent,
-  }
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
 ];
