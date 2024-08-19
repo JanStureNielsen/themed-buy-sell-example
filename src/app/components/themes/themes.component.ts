019e95ade116b9d51cd9be879483dcd50173ff30
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-19 17:37:40 -0600

    Add a type for theme modes.

--- src/app/components/themes/themes.component.ts
+++ src/app/components/themes/themes.component.ts
@@ -1,20 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import {FormsModule} from "@angular/forms";
-import {MatLabel} from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatRadioModule} from "@angular/material/radio";
+
+/**
+ * Angular Material Symbols:
+ *
+ *   light_mode -- light mode
+ *   dark_mode  -- dark mode
+ *   routine    -- system mode
+ */
+
+export type ThemeMode = {
+  icon: string;
+  mode: string;
+  label: string;
+};
 
 @Component({
   selector: 'fm-themes',
   standalone: true,
-    imports: [
-      FormsModule,
-      MatButtonToggleModule,
-      MatLabel
-    ],
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatRadioModule,
+    MatTooltip
+  ],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss'
 })
 export class ThemesComponent {
   lightTheme = false;
+
+  themeModes = signal<ThemeMode[]>([
+      {
+        icon: 'light_mode',
+        mode: 'light',
+        label: 'Light Mode',
+      },
+      {
+        icon: 'dark_mode',
+        mode: 'dark',
+        label: 'Dark Mode',
+      },
+      {
+        icon: 'routine',
+          mode: 'auto',
+        label: 'System Mode',
+      },
+      ]);
 
 }
