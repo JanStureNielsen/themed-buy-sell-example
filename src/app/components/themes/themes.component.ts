7cb1e13ca5d2471a62826c19dd0aa72dbf4346ec
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-19 07:16:31 -0600

    Add Angular Material label.

--- src/app/components/themes/themes.component.ts
+++ src/app/components/themes/themes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import {FormsModule} from "@angular/forms";
+import {MatLabel} from "@angular/material/form-field";
 
 @Component({
   selector: 'fm-themes',
@@ -8,7 +9,7 @@ import {FormsModule} from "@angular/forms";
     imports: [
       FormsModule,
       MatButtonToggleModule,
-
+      MatLabel
     ],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss'
