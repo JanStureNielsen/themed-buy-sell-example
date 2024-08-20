b569a7a4e48923e43dd1d41c8a80943892a179a2
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-19 18:39:21 -0600

    Add constants and document variables.

--- src/app/services/theme-manager.service.ts
+++ src/app/services/theme-manager.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
+
+export const THEME_PREFERENCE_LOCAL_STORAGE_KEY = 'themePreference';
+export const DARK_MODE_CLASS_NAME = 'docs-dark-mode';
+export const LIGHT_MODE_CLASS_NAME = 'docs-light-mode';
+export const PREFERS_COLOR_SCHEME_DARK = '(prefers-color-scheme: dark)';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ThemeManagerService {
+export class ThemeManager {
+  private readonly document = inject(DOCUMENT);
+  private readonly localStorage = inject(LOCAL_STORAGE);
+  private readonly platformId = inject(PLATFORM_ID);
 
   constructor() { }
 }
