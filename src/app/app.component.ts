a270b8e148c1da11f64742eeffca410a3edd1c80
Jan Nielsen <jan.sture.nielsen@gmail.com>
2025-01-13 11:32:46 -0700

    Remove components not used directly in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,6 @@ import { MatIconModule} from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { FooterComponent } from "./components/footer/footer.component";
-import { HeaderComponent } from "./components/header/header.component";
-import { OrderComponent } from "./components/order/order.component";
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 
 @Component({
@@ -21,9 +18,6 @@ import { SidenavComponent } from "./components/sidenav/sidenav.component";
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
-        FooterComponent,
-        HeaderComponent,
-        OrderComponent,
         SidenavComponent
     ],
     templateUrl: './app.component.html',
