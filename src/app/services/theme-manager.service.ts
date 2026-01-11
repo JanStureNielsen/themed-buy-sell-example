91987ae127055009787e7990941e5f9cecdfefc0
Jan Sture Nielsen <jan.sture.nielsen@gmail.com>
2026-01-11 03:48:19 +0000

    Update to Angular 20.

--- src/app/services/theme-manager.service.ts
+++ src/app/services/theme-manager.service.ts
@@ -1,5 +1,5 @@
-import {inject, Injectable, PLATFORM_ID} from '@angular/core';
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {inject, Injectable, PLATFORM_ID, DOCUMENT} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const THEME_PREFERENCE_LOCAL_STORAGE_KEY = 'themePreference';
 export const DARK_MODE_CLASS_NAME = 'docs-dark-mode';
