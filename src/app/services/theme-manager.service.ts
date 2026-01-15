42a17e39381132d2a44795eacb83d7ffb13391ac
Jan Nielsen <jan.sture.nielsen@gmail.com>
2026-01-15 17:25:45 +0000

    Remove color-scheme setting from theme name.

--- src/app/services/theme-manager.service.ts
+++ src/app/services/theme-manager.service.ts
@@ -20,7 +20,7 @@ export class ThemeManagerService {
   private readonly mediaQuery = window.matchMedia(PREFERS_COLOR_SCHEME_DARK);
 
   private readonly themes: Theme[] = [
-    { id: 'deep-blue-dark', primary: '#1976D2', displayName: 'Deep Blue Dark', },
+    { id: 'deep-blue', primary: '#1976D2', displayName: 'Deep Blue', },
     { id: 'green', primary: '#00796B', displayName: 'Green' },
     { id: 'orange', primary: '#E65100', displayName: 'Orange' },
     { id: 'purple', primary: '#6200EE', displayName: 'Purple' },
