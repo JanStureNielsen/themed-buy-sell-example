8bc4846cce5214e71e7cedc71efe37d1f01aca21
Jan Nielsen <jan.sture.nielsen@gmail.com>
2026-01-15 20:56:02 +0000

    Remove unused imports.

--- src/app/components/themes/themes.component.ts
+++ src/app/components/themes/themes.component.ts
@@ -1,14 +1,7 @@
 import {Component, inject, signal} from '@angular/core';
-import { FormsModule } from "@angular/forms";
 
 import { MatButtonToggleChange, MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatIconModule } from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatRadioModule} from "@angular/material/radio";
 import {ThemeManagerService, ThemeMode} from "../../services/theme-manager.service";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatMenuModule} from "@angular/material/menu";
 
 export interface ThemeModeOption {
   icon: string;
@@ -19,14 +12,7 @@ export interface ThemeModeOption {
 @Component({
     selector: 'fm-themes',
     imports: [
-        FormsModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatIconModule,
-        MatMenuModule,
-        MatRadioModule,
-        MatToolbarModule,
-        MatTooltipModule
+        MatButtonToggleModule
     ],
     templateUrl: './themes.component.html',
     styleUrl: './themes.component.scss'
